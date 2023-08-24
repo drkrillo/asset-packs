45c843bb1caa39da99d8f5603133d74dddc01102
Juan Cazala <juancazala@gmail.com>
2023-08-24 17:34:49 -0300

    chore: fix names

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import future from 'fp-future'
-import path from 'path'
+import { resolve, dirname } from 'path'
 
 export function exists(path: string) {
   const promise = future<boolean>()
@@ -16,55 +16,55 @@ export function exists(path: string) {
   return promise
 }
 
-export async function createDirectory(_path: string) {
-  if (await exists(_path)) {
+export async function createDirectory(path: string) {
+  if (await exists(path)) {
     return
   }
   const promise = future<void>()
-  fs.mkdir(_path, { recursive: true }, (err) =>
+  fs.mkdir(path, { recursive: true }, (err) =>
     err ? promise.reject(err) : promise.resolve(),
   )
   return promise
 }
 
-export async function writeFile(_path: string, data: string | Buffer) {
+export async function writeFile(path: string, data: string | Buffer) {
   const promise = future<void>()
-  const dir = path.dirname(_path)
+  const dir = dirname(path)
   await createDirectory(dir)
-  fs.writeFile(_path, data, 'utf8', (err) =>
+  fs.writeFile(path, data, 'utf8', (err) =>
     err ? promise.reject(err) : promise.resolve(),
   )
   return promise
 }
 
-export async function readJson(_path: string) {
+export async function readJson(path: string) {
   const promise = future<string>()
-  fs.readFile(_path, 'utf8', (err, data) =>
+  fs.readFile(path, 'utf8', (err, data) =>
     err ? promise.reject(err) : promise.resolve(data),
   )
   const data = await promise
   return JSON.parse(data)
 }
 
-export function readBuffer(_path: string) {
+export function readBuffer(path: string) {
   const promise = future<Buffer>()
-  fs.readFile(_path, (err, data) =>
+  fs.readFile(path, (err, data) =>
     err ? promise.reject(err) : promise.resolve(data),
   )
   return promise
 }
 
-export function isDirectory(_path: string) {
+export function isDirectory(path: string) {
   const promise = future<boolean>()
-  fs.lstat(_path, (err, stats) =>
+  fs.lstat(path, (err, stats) =>
     err ? promise.reject(err) : promise.resolve(stats.isDirectory()),
   )
   return promise
 }
 
-export function getSubfolders(_path: string) {
+export function getSubfolders(path: string) {
   const promise = future<string[]>()
-  fs.readdir(_path, async (err, files) => {
+  fs.readdir(path, async (err, files) => {
     if (err) {
       promise.reject(err)
     } else {
@@ -72,16 +72,22 @@ export function getSubfolders(_path: string) {
         files.map(async (file) => {
           return {
             file,
-            isDirectory: await isDirectory(path.resolve(_path, file)),
+            isDirectory: await isDirectory(resolve(path, file)),
           }
         }),
       )
       promise.resolve(
         results
           .filter((result) => result.isDirectory)
-          .map((result) => path.resolve(_path, result.file)),
+          .map((result) => resolve(path, result.file)),
       )
     }
   })
+  return promise
+}
+
+export function move(from: string, to: string) {
+  const promise = future<void>()
+  fs.rename(from, to, (err) => (err ? promise.reject(err) : promise.resolve()))
   return promise
 }
