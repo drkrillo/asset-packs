e535bdaea6b968bb0fdff2bb8bbd18eaff92323c
Juan Cazala <juancazala@gmail.com>
2023-08-24 17:43:40 -0300

    fix: added non-smart and removed smart

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -1,16 +1,18 @@
-import { rimraf } from 'rimraf'
 import { LocalFileSystem } from '../src/local'
+import { exists } from '../src/utils/fs'
 
 async function main() {
   const local = new LocalFileSystem('./assets')
   const assetPacks = await local.getAssetPacks()
   for (const assetPack of assetPacks) {
     const assetPackPath = local.getAssetsPath(assetPack.name)
-    const assets = await local.getAssets(assetPackPath)
-    for (const asset of assets) {
-      console.log(asset.name, '✅')
+    if (await exists(assetPackPath)) {
+      const assets = await local.getAssets(assetPackPath)
+      for (const asset of assets) {
+        console.log(asset.name, '✅')
+      }
+      console.log(assetPack.name, '✅')
     }
-    console.log(assetPack.name, '✅')
   }
 }
 
