9be670c7b80c865889545f21fc84005614d53f97
Juan Cazala <juancazala@gmail.com>
2023-09-20 14:18:16 -0300

    feat: added events

---
+++ src/events.ts
@@ -0,0 +1,21 @@
+import mitt, { Emitter } from 'mitt'
+import { Entity } from '@dcl/sdk/ecs'
+import { Action, TriggerType } from './definitions'
+
+const triggers = new Map<Entity, Emitter<Record<TriggerType, void>>>()
+
+const actions = new Map<Entity, Emitter<Record<string, Action['payload']>>>()
+
+export function getTriggerEvents(entity: Entity) {
+  if (!triggers.has(entity)) {
+    triggers.set(entity, mitt())
+  }
+  return triggers.get(entity)!
+}
+
+export function getActionEvents(entity: Entity) {
+  if (!actions.has(entity)) {
+    actions.set(entity, mitt())
+  }
+  return actions.get(entity)!
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import {  engine } from '@dcl/sdk/ecs'
+import { name, version } from '../package.json'
 import { actionsSystem } from './actions'
 import { triggersSystem } from './triggers'
 
 export function main() {
+  console.log(`Using ${name}@${version}`)
   engine.addSystem(actionsSystem)
   engine.addSystem(triggersSystem)
 }
