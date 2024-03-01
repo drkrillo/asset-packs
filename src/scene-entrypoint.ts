f3c9c3e8e2fad39f6aa2ee3bebf710d4e2c57e53
Gon Pombo <gonzalo@decentraland.org>
2024-03-01 18:39:49 -0300

    avoid duplicating systems if the lib is called more than once (#96)

--- src/scene-entrypoint.ts
+++ src/scene-entrypoint.ts
@@ -8,11 +8,17 @@ import { createTriggersSystem } from './triggers'
 import { createTimerSystem } from './timer'
 import { getExplorerComponents } from './components'
 
+let initialized: boolean = false
 /**
  * the _args param is there to mantain backwards compatibility with all versions.
  * Before it was initAssetPacks(engine, pointerEventsSystem, components)
  */
 export function initAssetPacks(_engine: unknown, ..._args: any[]) {
+  // Avoid creating the same systems if asset-pack is called more than once
+  if (initialized) return
+  initialized = true
+  // .
+
   const engine = _engine as IEngine
   try {
     const components = getExplorerComponents(engine)
